b1e9564e25b2e89f9bb97afab3ac76c1eb31e3c9
NataliiaOsintseva <nataly.osya@gmail.com>
2016-11-10 17:38:33 +0200

    added gitignire

---
+++ Store/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ninject;
+using System.Web.Mvc;
+
+namespace Store.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelArg)
+        {
+            kernel = kernelArg;
+            AddBindings();            
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        public void AddBindings()
+        {
+
+        }
+    }
+}
